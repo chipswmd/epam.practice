a15b491089cab30b061d47ef0adb3b774f9ee474
= <=>
2021-02-15 19:17:03 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 apt-get update
 apt-get install git
-git clone https://github.com/chipswmd/epam.practice vagrantdir
+git clone https://github.com/chipswmd/epam.practice.git task2
+cd task2
 git checkout task2
 cat bootstrap.sh
 ping vm2
